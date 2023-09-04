8c17b6ecde13033a1d2287d17f43480f121c609e
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 18:57:04 -0300

    Add files via upload

---
+++ ProgramEx3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX3POOMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da diagonal do quadrado: ");
+
+            Double valorDiagonal = Double.Parse(Console.ReadLine());
+
+
+
+            Quadrado quadrado = new Quadrado(valorDiagonal);
+
+            Double area = quadrado.CalcularArea();
+
+
+
+            Console.WriteLine("A área do quadrado é: {0}", area);
+        }
+    }
+}
